007aa1a1abf325aa19e697965b445f2b7592ba4f
eladg <ggrelad@gmail.com>
2016-01-21 09:40:12 -0800

    added POST /list (see descriptions)
    added POST /list, return CSV for the list of orders in queue
    added validate_admin_token!
    changed job payload to include IP, timestamp (see discussion on:
    https://midburnos.slack.com/archives/secondsale/p1453391566000263)

--- resque_me.rb
+++ resque_me.rb
@@ -5,6 +5,7 @@ require "dotenv"; Dotenv.load
 # application space
 require "redis"
 require "resque"
+require "csv"
 require "./worker.rb"
 
 TIER_SIZE = ENV["QUEUE_TIER_SIZE"].to_i
@@ -15,43 +16,68 @@ class ResqueMe < Sinatra::Base
     JSON.parse(request.body.read)
   end
 
+  def validate_admin_token!
+    unauthorized unless get_params["admin_secret_token"] == ENV["ADMIN_SECRET_TOKEN"]
+  end
+
+  def unauthorized
+    puts "============= UNAUTHORIZED! =============="
+    puts "unauthorized request from #{request.ip}"
+    puts "=========================================="
+    halt 403
+  end
+
   get '/' do
     redirect "http://midburn.org"
   end
 
-  post '/big-reset' do
-    payload = get_params
-    if payload["admin_secret_token"] == ENV["ADMIN_SECRET_TOKEN"]
-      logger.info "Performing big reset. Removing all tasks from all queues!"
-      queues = Resque.queues
-      queues.each do |queue_name|
-        logger.info "Clearing #{queue_name}..."
-        Resque.remove_queue "#{queue_name}"
-        Resque.redis.del "queue:#{queue_name}"
+  post '/list' do
+    validate_admin_token!
+    
+    data = []
+    Resque.queues.each do |queue|
+      Resque.peek(queue, 0, TIER_SIZE).find_all do |job| 
+        args = JSON.parse(job["args"][0])
+        data << [ queue, args["ip"], args["timestamp"], args["email"] ] # https://midburnos.slack.com/archives/secondsale/p1453391566000263
       end
-      
-      logger.info "Clearing delayed..." # in case of scheduler - doesn't break if no scheduler module is installed
-      Resque.redis.keys("delayed:*").each do |key|
-        Resque.redis.del "#{key}"
-      end
-      Resque.redis.del "delayed_queue_schedule"
-      
-      logger.info "Clearing stats..."
-      Resque.redis.set "stat:failed", 0 
-      Resque.redis.set "stat:processed", 0
-      logger.info "Done."
-    else
-      logger.error "tried to reset DB with wrong admin secret token!"
     end
 
+    CSV.generate do |csv|
+      data.each do |row|
+        csv << row
+      end
+    end
+  end
+
+  post '/big-reset' do
+    validate_admin_token!
+
+    puts "========== PERFORMING BIG RESET =========="
+    puts "Performing big reset. Removing all tasks from all queues!"
+    queues = Resque.queues
+    queues.each do |queue_name|
+      puts "Clearing #{queue_name}..."
+      Resque.remove_queue "#{queue_name}"
+      Resque.redis.del "queue:#{queue_name}"
+    end
+    
+    puts "Clearing delayed..." # in case of scheduler - doesn't break if no scheduler module is installed
+    Resque.redis.keys("delayed:*").each do |key|
+      Resque.redis.del "#{key}"
+    end
+    Resque.redis.del "delayed_queue_schedule"
+    
+    puts "Clearing stats..."
+    Resque.redis.set "stat:failed", 0 
+    Resque.redis.set "stat:processed", 0
+    puts "Done."
+    puts "=========================================="
   end
 
   post '/enqueue' do
     payload = get_params
-    order_json = %{{"firstname":"#{payload["firstname"]}","lastname":"#{payload["lastname"]}","email":"#{payload["email"]}"}}
     order_json = %{{"ip":"#{request.ip}","timestamp":"#{Time.now.to_i}","email":"#{payload["email"]}"}}
 
-    tier_number = (Resque.info[:pending] + Resque.info[:processed]).div TIER_SIZE.to_i
     tier_number = (Resque.info[:pending] + Resque.info[:processed]).div TIER_SIZE
     Resque.enqueue(eval("OrderTier_#{tier_number}"), order_json)
   end
