60d45e3e834881091a2b50a636ab481b8a4f6a10
eladg <ggrelad@gmail.com>
2016-01-28 19:37:12 -0800

    update worker for single queue

--- worker.rb
+++ worker.rb
@@ -1,19 +1,7 @@
-lambda { 
-  x = -1
-  Kernel.send(:define_method, :get_tier_number) {
-    x += 1
-    x.to_s.rjust(3, '0')
-  }
-}.call
-
-500.times do |tier_number|
-  Object.const_set("OrderTier_#{tier_number}", Class.new do
-      @queue = :"tier_#{get_tier_number}"
-      def self.perform(json) 
-        process_order(JSON.parse(json)) 
-      end 
-    end
-  )
+class TicketsQueue
+  @queue = :tickets_queue
+  def self.perform(json)
+  end
 end
 
 class BannedOrder
