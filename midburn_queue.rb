9f1ac1164a7245a6007dc210270c44ab8f13242e
eladg <ggrelad@gmail.com>
2016-02-15 12:26:17 -0800

    Env Vars for: - Register URL (default: 'register.html') - Email param for register route: (default is 'email' so something like: {"email":"user@email.com"}) - Register route itself (default: '/register') - Redis Key for Queue is open, so multiple envs can use the same DB (default: 'queue_is_open')

--- midburn_queue.rb
+++ midburn_queue.rb
@@ -10,6 +10,13 @@ require "resque"
 require "csv"
 require "./worker.rb"
 
+# App Configurations
+QUEUE_IS_OPEN_REDIS_KEY     = ENV["QUEUE_IS_OPEN_REDIS_KEY"]  || "queue_is_open"
+USERS_EMAIL_PARAM           = ENV["USERS_EMAIL_PARAM"] || "email"
+REGISTER_FORM_URL           = ENV["REGISTER_FORM_URL"] || "register.html"
+ACCESS_CONTROL_ALLOW_ORIGIN = ENV["ACCESS_CONTROL_ALLOW_ORIGIN"] || "*"
+REGISTER_ROUTE              = ENV["REGISTER_ROUTE"] || "/register"
+
 class MidburnQueue < Sinatra::Base
   configure do
     enable :cross_origin
@@ -21,12 +28,19 @@ class MidburnQueue < Sinatra::Base
   end
 
   before :method => :post do
-    # http://stackoverflow.com/questions/15671006/before-filter-for-all-post-requests-in-sinatra
-    response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
-    response.headers["Access-Control-Allow-Origin"] = ENV["ACCESS_CONTROL_ALLOW_ORIGIN"]
-
     load_params
+    set_response_headers
+    access_log
   end 
+
+  def access_log
+    puts "[access log] #{self.env["REQUEST_METHOD"]} #{self.env["REQUEST_PATH"]} from #{self.env["REMOTE_ADDR"]}: #{params}"
+  end
+
+  def set_response_headers
+    response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+    response.headers["Access-Control-Allow-Origin"] = ACCESS_CONTROL_ALLOW_ORIGIN
+  end
 
   def load_params
     return @params if @request_body_read
@@ -39,26 +53,23 @@ class MidburnQueue < Sinatra::Base
   end
 
   def queue_is_open?
-    Resque.redis.get("queue_is_open") == "true"
+    Resque.redis.get(QUEUE_IS_OPEN_REDIS_KEY) == "true"
   end
 
   options "*" do
+    set_response_headers
     response.headers["Allow"] = "HEAD,POST,OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
-    response.headers["Access-Control-Allow-Origin"] = ENV["ACCESS_CONTROL_ALLOW_ORIGIN"]
     200
   end
 
   post '/status' do
     halt(403) if not queue_is_open?
-    { register_page: ENV["REGISTER_FORM_URL"] }.to_json
+    { register_page: REGISTER_FORM_URL }.to_json
   end
 
-  post '/register' do
-    halt(400) if params["username"].empty?
-
-    puts "[access log] POST: #{params}" # put a little access log
-    order_json = %{{"ip":"#{request.ip}","timestamp":"#{Time.now.to_i}","email":"#{params["username"]}"}}
+  post "#{REGISTER_ROUTE}" do
+    halt(400) if params[USERS_EMAIL_PARAM].empty?
+    order_json = %{{"ip":"#{request.ip}","timestamp":"#{Time.now.to_i}","email":"#{params[USERS_EMAIL_PARAM]}"}}
 
     if queue_is_open?
       Resque.enqueue(TicketsQueue, order_json)
